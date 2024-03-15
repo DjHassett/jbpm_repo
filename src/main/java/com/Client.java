eb7a578b61bbe1662a18dcb26cc6dc5775951063
wbadmin <>
2024-03-15 11:36:36 +0000

    {/src/main/java/com/Client.java}

--- src/main/java/com/Client.java
+++ src/main/java/com/Client.java
@@ -6,12 +6,54 @@ package com;
 
 public class Client implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Client() {
-    }
+	private java.lang.Integer age;
+	private java.lang.String firstName;
+	private java.lang.String surname;
+	private java.lang.Boolean consented;
 
+	public Client() {
+	}
 
+	public java.lang.Integer getAge() {
+		return this.age;
+	}
 
+	public void setAge(java.lang.Integer age) {
+		this.age = age;
+	}
+
+	public java.lang.String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(java.lang.String firstName) {
+		this.firstName = firstName;
+	}
+
+	public java.lang.String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(java.lang.String surname) {
+		this.surname = surname;
+	}
+
+	public java.lang.Boolean getConsented() {
+		return this.consented;
+	}
+
+	public void setConsented(java.lang.Boolean consented) {
+		this.consented = consented;
+	}
+
+	public Client(java.lang.Integer age, java.lang.String firstName,
+			java.lang.String surname, java.lang.Boolean consented) {
+		this.age = age;
+		this.firstName = firstName;
+		this.surname = surname;
+		this.consented = consented;
+	}
 
 }
